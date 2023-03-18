eaa513a42ff8b82db033a7f76d8c7a127eda5a96
Abhishek Kumar <abhishekkumar@Abhisheks-Air.bbrouter>
2023-03-18 10:55:33 +0530

    adding swagger specs for documentation

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express =require("express")
 const app= express();
-
-app.use(express.json());
+const cors = require("cors");
 const errorMiddleware=require("./middleware/error")
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:4000",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+const specs = swaggerJsDoc(options);
+app.use(cors());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use(express.json());
 // //route imports
 const item=require("./routes/itemRoute.js");
 app.use("/api/v1",item);
--- routes/itemRoute.js
+++ routes/itemRoute.js
@@ -2,13 +2,146 @@ const express = require("express");
 const { getAllItem, createItem, updateItem, deleteItem } = require("../controllers/itemController");
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Item:
+ *       type: object
+ *       required:
+ *         - name
+ *         - price
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the Item
+ *         name:
+ *           type: string
+ *           description: name of item
+ *         price:
+ *           type: string
+ *           description:  Item price
+ *         description: 
+ *            type: string
+ *             description: Item descriptions
+ *       example:
+ *         id: d5fE_asz
+ *         name: Item1
+ *         description:this is item1 
+ *         price:10
+ * 
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Items
+  *   description: The Item managing API
+  */
+
+
+ /**
+ * @swagger
+ * /api/v1/items:
+ *   get:
+ *     summary: Returns the list of all the itmes
+ *     tags: [Items]
+ *     responses:
+ *       200:
+ *         description: The list of the Items
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Item'
+ */
 router.route("/items").get(getAllItem);
+
+
+/**
+ * @swagger
+ * /api/v1/item/new:
+ *   post:
+ *     summary: Create a new Item
+ *     tags: [Items]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Item'
+ *     responses:
+ *       200:
+ *         description: The Item was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Item'
+ *       500:
+ *         description: Some server error
+ */
 router
   .route("/item/new")
   .post( createItem);
+
+
+  /**
+ * @swagger
+ * /api/v1/item/{id}:
+ *  put:
+ *    summary: Update the Item by the id
+ *    tags: [Items]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The Item id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Item'
+ *    responses:
+ *      200:
+ *        description: The Item was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Item'
+ *      404:
+ *        description: The Item was not found
+ *      500:
+ *        description: Some error happened
+ */
 router
   .route("/item/:id")
   .put( updateItem);
+
+
+  /**
+ * @swagger
+ * /api/v1/item/{id}:
+ *   delete:
+ *     summary: Remove the Item by id
+ *     tags: [Items]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Item id
+ * 
+ *     responses:
+ *       200:
+ *         description: The Item was deleted
+ *       404:
+ *         description: The Item was not found
+ */
 router
   .route("/item/:id")
   .delete( deleteItem);
